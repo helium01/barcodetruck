0a72d732b8450ac9599c5bcfbb0f06ce92c9f5e9
helium01 <fajarw935@gmail.com>
2024-01-18 21:18:59 +0700

    penambahan

--- resources/views/awal.blade.php
+++ resources/views/awal.blade.php
@@ -39,10 +39,28 @@
           </div>
     </div>
     <h1>QR Code Scanner</h1>
-    <video id="preview"></video>
+    <div id="reader" width="600px"></div>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-    <script src="http://localhost:8000/assets/instascan.min.js"></script>
-    {{-- <video id="preview"></video> --}}
+    {{-- <script src="http://localhost:8000/assets/instascan.min.js"></script> --}}
+    <script src="https://unpkg.com/html5-qrcode" type="text/javascript"> </script>
+    <script>
+    function onScanSuccess(decodedText, decodedResult) {
+  // handle the scanned code as you like, for example:
+  console.log(`Code matched = ${decodedText}`, decodedResult);
+}
+
+function onScanFailure(error) {
+  // handle scan failure, usually better to ignore and keep scanning.
+  // for example:
+  console.warn(`Code scan error = ${error}`);
+}
+
+let html5QrcodeScanner = new Html5QrcodeScanner(
+  "reader",
+  { fps: 10, qrbox: {width: 250, height: 250} },
+  /* verbose= */ false);
+html5QrcodeScanner.render(onScanSuccess, onScanFailure);
+    </script>
     <script type="text/javascript">
       let scanner = new Instascan.Scanner({ video: document.getElementById('preview') });
       scanner.addListener('scan', function (content) {
